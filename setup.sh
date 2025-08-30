f7bcc2b8c566151178018357656396c9d9df8e4d
Winston <23213132+winston-w@users.noreply.github.com>
2025-08-30 18:37:07 +0200

    add: tmux-logging

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 
+mkdir ~/.tmux
+mkdir ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tmux-logging.git ~/.tmux/plugins/
 
 git clone https://github.com/Dewalt-arch/pimpmykali ~
 cd ~/pimpykali
