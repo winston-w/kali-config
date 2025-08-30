72298de1ce2d91c95f2e8719a9389eb633ac0250
Winston <23213132+winston-w@users.noreply.github.com>
2025-08-30 18:44:24 +0200

    fix file paths

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 
 mkdir ~/.tmux
 mkdir ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tmux-logging.git ~/.tmux/plugins/t
+git clone https://github.com/tmux-plugins/tmux-logging.git ~/.tmux/plugins/tmux-logging
 
-git clone https://github.com/Dewalt-arch/pimpmykalii.git ~/
+git clone https://github.com/Dewalt-arch/pimpmykalii.git ~/pimpmykali
 cd ~/pimpykali
 sudo ./pimpmykali.sh
