370b1ea1853db1d99253ec604d6aa7307ffc61c4
Winston <23213132+winston-w@users.noreply.github.com>
2025-08-30 18:43:07 +0200

    fix typo

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ mkdir ~/.tmux
 mkdir ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tmux-logging.git ~/.tmux/plugins/t
 
-git clone https://github.com/Dewalt-arch/pimpmykalii.git ~
+git clone https://github.com/Dewalt-arch/pimpmykalii.git ~/
 cd ~/pimpykali
 sudo ./pimpmykali.sh
